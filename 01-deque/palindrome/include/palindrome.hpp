989191cdf563f9749f30741c1059b195f3d8de54
vagrant <vagrant@vagrant.vm>
2020-10-03 18:22:12 +0000

    build 2

--- 01-deque/palindrome/include/palindrome.hpp
+++ 01-deque/palindrome/include/palindrome.hpp
@@ -14,9 +14,9 @@ private:
 
 template<typename Deque>
 bool Palindrome<Deque>::is_palindrome(const std::string& s1) {
-    int a = 1;
-    for (int i = 0; i < s1.size() / 2; i++) if (s1[i] != s1[s1.size() - i - 1]) a = 0;
-    if (a) return true;
+    int a = 0;
+    for (int i = 0; i < s1.size() / 2; i++) if (s1[i] != s1[s1.size() - i - 1]) a = 1;
+    if (!a) return true;
     return false;
 }
 
